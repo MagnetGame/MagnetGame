a328a33e2a7874b4f2fff5daacf0124592fd7e92
Dante Barbosa-Toney <dtoney136@gmail.com>
2023-10-30 13:20:50 -0400

    Lines
    Attempting to implement some kind of line to indicate when you will interact with an object of N/S Polarity

--- Assets/Scripts/PlayerMagnetism.cs
+++ Assets/Scripts/PlayerMagnetism.cs
@@ -1,13 +1,15 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMagnetism : MonoBehaviour
 {
     public float magnetRadius = 5f;
-    public float magnetForce = 10f;
+    public float magnetForce = 2f;
+    public LineRenderer lineRendererPrefab;
+    private LineRenderer currentLineRenderer;
     private bool isAttracting = false;
     private bool isRepelling = false;
+    private Rigidbody2D rb;
+    
 
     void Update()
     {
@@ -37,24 +39,48 @@ public class PlayerMagnetism : MonoBehaviour
                 if ((isAttracting && collider.CompareTag("NorthPolarity")) || (isRepelling && collider.CompareTag("SouthPolarity")))
                 {
                     Debug.Log("North Mode");
-                    Rigidbody2D rb = collider.GetComponent<Rigidbody2D>();
+                    DrawLine(collider.transform.position, Color.red);
+
+                    rb = collider.GetComponent<Rigidbody2D>();
                     if (rb != null)
                     {
                         Vector2 direction = transform.position - collider.transform.position;
-                        rb.AddForce(direction.normalized * magnetForce, ForceMode2D.Impulse);
+                        rb.AddForce(direction.normalized * 10f, ForceMode2D.Impulse);
                     }
                 }
                 else if ((isAttracting && collider.CompareTag("SouthPolarity")) || (isRepelling && collider.CompareTag("NorthPolarity")))
                 {
-                    Rigidbody2D rb = collider.GetComponent<Rigidbody2D>();
+                    Debug.Log("South Mode");
+                    DrawLine(collider.transform.position, Color.blue);
+
+                    rb = collider.GetComponent<Rigidbody2D>();
                     if (rb != null)
                     {
-                        Debug.Log("South Mode");
                         Vector2 direction = collider.transform.position - transform.position;
-                        rb.AddForce(direction.normalized * magnetForce, ForceMode2D.Impulse);
+                        rb.AddForce(direction.normalized * 10f, ForceMode2D.Impulse);
                     }
                 }
             }
         }
+        else
+        {
+            if (currentLineRenderer != null)
+            {
+                Destroy(currentLineRenderer.gameObject);
+            }
+        }
+    }
+
+    void DrawLine(Vector3 targetPosition, Color color)
+    {
+        if (currentLineRenderer == null)
+        {
+            currentLineRenderer = Instantiate(lineRendererPrefab, transform.position, Quaternion.identity);
+        }
+
+        currentLineRenderer.startColor = color;
+        currentLineRenderer.endColor = color;
+        currentLineRenderer.SetPosition(0, transform.position);
+        currentLineRenderer.SetPosition(1, targetPosition);
     }
 }
